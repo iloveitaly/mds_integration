db2a0de4762a1adc62e819800f000c95da8cc1b8
Pablo Cantero <pablo@pablocantero.com>
2014-10-27 11:27:53 -0200

    IN-663 Use dotenv instead of credentials.yml

--- spec/support/sample_objects.rb
+++ spec/support/sample_objects.rb
@@ -1,17 +1,9 @@
 def sample_credentials
-  saved_credentials.merge({ "test_mode"=>"1" }).with_indifferent_access
-end
-
-def saved_credentials
-  begin
-    # Add your testing credentials to credentials.yml
-    YAML.load_file("credentials.yml")
-  rescue Errno::ENOENT
-    {
-      "client_code"=>"client_code",
-      "client_signature"=>"client_signature"
-    }
-  end
+  {
+    "test_mode"        => '1',
+    "client_code"      => ENV['CLIENT_CODE'],
+    "client_signature" => ENV['CLIENT_SIGNATURE']
+  }.with_indifferent_access
 end
 
 def sample_shipment(number = "R#{rand(999999)}")
@@ -23,40 +15,40 @@ def sample_shipment(number = "R#{rand(999999)}")
     "currency"=>"USD",
     "placed_on"=>"2014-02-03T17:29:15.219Z",
     "totals"=>
-      {"item"=>200,
-        "adjustment"=>20,
-        "tax"=>10,
-        "shipping"=>10,
-        "payment"=>220,
-        "order"=>220},
-    "items"=>
-      [{"product_id"=>"QTRZIM03",
-        "name"=>"QTRZIM03",
-        "quantity"=>2,
-        "price"=>100}],
-        "adjustments"=>
-          [{"name"=>"Tax", "value"=>10},
-            {"name"=>"Shipping", "value"=>5},
-            {"name"=>"Shipping", "value"=>5}],
+    {"item"=>200,
+     "adjustment"=>20,
+     "tax"=>10,
+     "shipping"=>10,
+     "payment"=>220,
+     "order"=>220},
+     "items"=>
+    [{"product_id"=>"QTRZIM03",
+      "name"=>"QTRZIM03",
+      "quantity"=>2,
+      "price"=>100}],
+      "adjustments"=>
+    [{"name"=>"Tax", "value"=>10},
+     {"name"=>"Shipping", "value"=>5},
+     {"name"=>"Shipping", "value"=>5}],
     "shipping_address"=>
-      {"firstname"=>"Joe",
-        "lastname"=>"Smith",
-        "address1"=>"1234 Awesome Street",
-        "address2"=>"",
-        "zipcode"=>"90210",
-        "city"=>"Hollywood",
-        "state"=>"California",
-        "country"=>"US",
-        "phone"=>"0000000000"},
-    "billing_address"=>
-      {"firstname"=>"Joe",
-        "lastname"=>"Smith",
-        "address1"=>"1234 Awesome Street",
-        "address2"=>"",
-        "zipcode"=>"90210",
-        "state"=>"California",
-        "country"=>"US",
-        "phone"=>"0000000000"}
+    {"firstname"=>"Joe",
+     "lastname"=>"Smith",
+     "address1"=>"1234 Awesome Street",
+     "address2"=>"",
+     "zipcode"=>"90210",
+     "city"=>"Hollywood",
+     "state"=>"California",
+     "country"=>"US",
+     "phone"=>"0000000000"},
+     "billing_address"=>
+    {"firstname"=>"Joe",
+     "lastname"=>"Smith",
+     "address1"=>"1234 Awesome Street",
+     "address2"=>"",
+     "zipcode"=>"90210",
+     "state"=>"California",
+     "country"=>"US",
+     "phone"=>"0000000000"}
   }.merge(order_extra_fields).with_indifferent_access
 end
 
