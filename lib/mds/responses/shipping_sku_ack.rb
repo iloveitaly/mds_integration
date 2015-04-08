9a8ac6dbb8d3328f48a46f509bf1c78d9375b982
Michael Bianco <iloveitaly@gmail.com>
2015-04-08 10:47:42 -0400

    Ensure that OrderID is unique in /get_shipments response

--- lib/mds/responses/shipping_sku_ack.rb
+++ lib/mds/responses/shipping_sku_ack.rb
@@ -18,14 +18,16 @@ module MDS
       def objects
         orders = body['Order'].is_a?(Hash) ? [body['Order']] : body['Order']
 
-        orders.to_a.sort_by { |o| o["TrackingNumber"] }.map do |order|
-          {
+        orders.to_a.sort_by { |o| o["TrackingNumber"] }.inject({}) do |h, order|
+          h[order["OrderID"]] = {
             id: order["OrderID"],
             status: "shipped",
             tracking: order["TrackingNumber"],
             shipped_at: parse_date(order["OrderShipDate"])
           }
-        end
+
+          h
+        end.values
       end
 
       def parse_date(date)
